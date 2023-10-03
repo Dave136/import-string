f7cf54123e50ed5e8016e8f8348077e43d3c92a1
David Arenas <davejs136@gmail.com>
2023-10-02 20:24:14 -0400

    feat: support for bun, deno and node for import code from string

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,35 @@
-import { transformSync } from 'esbuild';
+import { importFromStringSync } from 'module-from-string';
+import { transformSync } from '@swc/wasm';
+
+const isBun = () => 'Bun' in globalThis;
+const isDeno = () => 'Deno' in globalThis;
+const isNode = () => !isBun() && !isDeno();
 
 export async function importModule(srcCode: string) {
-  const { code } = transformSync(srcCode, {
-    loader: 'ts',
-    target: 'es2015',
-  });
+  if (isDeno()) {
+    const { code } = transformSync(srcCode, {
+      jsc: {
+        target: 'es2018',
+        parser: {
+          syntax: 'typescript',
+        }
+      },
+    });
+  
+    const toBase64 = (str: string) => `data:text/javascript;base64,${btoa(str)}`;
+    const module = await import(toBase64(code));
 
-  const toBase64 = (str: string) => `data:text/javascript;base64,${btoa(str)}`;
-  const module = await import(toBase64(code));
+    return module;
+  }
 
-  return module;
+  if (isNode() || isBun()) {
+    const mod = importFromStringSync(srcCode, {
+      transformOptions: {
+        loader: 'ts',
+        target: 'es2018',
+      }
+    });
+
+    return mod;
+  }
 }
